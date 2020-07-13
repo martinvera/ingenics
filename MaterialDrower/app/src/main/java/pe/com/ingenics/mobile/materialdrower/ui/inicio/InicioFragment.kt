10ad2eca9e604958ce451303a0f35e0c0769294d
saulchirinosarista <saulchirinosarista@gmail.com>
2020-07-13 13:43:21 -0500

    MiultimoDesarrollo

--- MaterialDrower/app/src/main/java/pe/com/ingenics/mobile/materialdrower/ui/inicio/InicioFragment.kt
+++ MaterialDrower/app/src/main/java/pe/com/ingenics/mobile/materialdrower/ui/inicio/InicioFragment.kt
@@ -1,10 +1,14 @@
 package pe.com.ingenics.mobile.materialdrower.ui.inicio
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridLayout
 import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -19,13 +23,26 @@ class InicioFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        inicioViewModel =
-                ViewModelProviders.of(this).get(InicioViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_inicio, container, false)
-       // val textView: TextView = root.findViewById(R.id.text_inicio)
+        /*
+        inicioViewModel = ViewModelProviders.of(this).get(InicioViewModel::class.java)
         inicioViewModel.text.observe(viewLifecycleOwner, Observer {
-           // textView.text = it
+            //   textView.text = it
         })
+
+         */
+
+        val root = inflater.inflate(R.layout.fragment_inicio, container, false)
+        val cdAgregados: CardView = root.findViewById(R.id.cdAgregados)
+        cdAgregados.setOnClickListener {
+
+            val gridContenedor:GridLayout=root.findViewById(R.id.gridContenedor)
+            gridContenedor.removeAllViews()
+
+
+
+            Toast.makeText(activity,"Agregados", Toast.LENGTH_SHORT).show()
+        }
+
         return root
     }
 }
